14f083402df35eddc6df08ac65069ebdda866b13
sonlamkk011 <sonlamkk011@gmail.com>
2021-06-10 11:05:16 +0700

    bai1

---
+++ Baithi/MobilePhone.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Ex1
+{
+    public class MobilePhone : Phone
+    {
+      
+    }
+}   
---
+++ Baithi/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Ex1;
+
+namespace Baithi
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MobilePhone mobilePhone = new MobilePhone()
+            mobilePhone.PhoneName = "Iphonelamson";
+            mobilePhone.PhoneType = "mobile";
+            mobilePhone.PhonePrice = 2001;
+            mobilePhone.display();
+
+            Phone phone = new Phone();
+            phone.PhoneName = "Iphone11";
+            phone.PhoneType = "phone";
+            phone.PhonePirce = 2001;
+            phone.display();
+        }
+    }
+}
